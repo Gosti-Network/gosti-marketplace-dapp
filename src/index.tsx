3561e54a2f9024f960704c32754c2671f927b1aa
Gaerax <gaerax@gmail.com>
2023-02-14 13:38:40 -0800

    removed testing shit

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles';
@@ -47,7 +46,3 @@ root.render(
 	</React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
